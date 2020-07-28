1c6c99eade58ec5249138c299b57ba2ab561b658
ChinmayaMishra98 <68907648+ChinmayaMishra98@users.noreply.github.com>
2020-07-28 18:30:10 +0530

    Delete cartStatus.js

--- cartStatus.js
+++
@@ -1,7 +0,0 @@
-function addToCart() {
-    document.getElementById("add-status").innerHTML="Item added to Cart Successfully";
-}
-
-function deleteFromCart() {
-    document.getElementById("delete-status").innerHTML="Item removed from Cart Successfully";
-}
