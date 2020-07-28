20c5e1e7a8521bc6271edb7139ce06e767c14e74
ChinmayaMishra98 <68907648+ChinmayaMishra98@users.noreply.github.com>
2020-07-28 18:08:32 +0530

    Add files via upload

---
+++ cartStatus.js
@@ -0,0 +1,7 @@
+function addToCart() {
+    document.getElementById("add-status").innerHTML="Item added to Cart Successfully";
+}
+
+function deleteFromCart() {
+    document.getElementById("delete-status").innerHTML="Item removed from Cart Successfully";
+}
